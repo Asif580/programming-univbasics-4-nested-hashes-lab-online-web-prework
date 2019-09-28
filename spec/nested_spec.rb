ddd278a54d3405b84a843c1ed210a774f049adc1
Asiful Choudhury <asiful307@gmail.com>
2019-09-28 22:00:58 +0000

    Automatically backed up by Learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -8,7 +8,11 @@ describe "nested hash" do
 
     end
   end
-programmer_hash[:grace_hopper] 
+def hopper
+  programmer_hash = {
+    grace_hopper: ""
+  }
+end
   describe "alan_kay_is_known_for" do
     it "operates on the programmer_hash and returns the value of what Alan Kay is known for" do
       expect(alan_kay_is_known_for).to eq("Object Orientation")
